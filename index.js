86fdd2f6abc7e78ec532537ec8dfb8219fbd2bb4
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-09 12:14:49 -0300

    Utilizando o metodo POST

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express();
 
 app.listen('3000')
 
-app.route('/').get((request, response) => response.send("Hello"))
-app.route('/sobre').get((request, response) => response.send("Sobre"))
+//middleware
+app.use(express.json())
+
+app.route('/').post((request, response) => response.send(request.body));

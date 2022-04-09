8aa0930f5bc1e5123276583c7011e1cce0f4547b
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-09 11:52:37 -0300

    Utilizando o metodo GET

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const express = require('express');
 const app = express();
 
 app.listen('3000')
+
+app.route('/').get((request, response) => response.send("Hello"))
+app.route('/sobre').get((request, response) => response.send("Sobre"))
